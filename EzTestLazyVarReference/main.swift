10704899b798d2d65e6e04faa93ed2a00c2dd9ae
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-03-11 10:20:50 +0900

    Initial Commit

---
+++ EzTestLazyVarReference/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EzTestLazyVarReference
+//
+//  Created by Tomohiro Kumagai on 3/11/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
